6a1e7f4689b64505e25fbddefb75fcbb6c615584
masshirodev <masshiro.iro.kk@gmail.com>
2021-01-29 17:35:16 -0400

    Adding FSH profile

---
+++ Gather/Madao/Mash-5.41 Diadem Artisanal FSH.lua
@@ -0,0 +1,215 @@
+local tbl = 
+{
+	eph = 
+	{
+	},
+	ephs = 
+	{
+		[0] = 
+		{
+		},
+		[4] = 
+		{
+		},
+		[8] = 
+		{
+		},
+		[12] = 
+		{
+		},
+		[16] = 
+		{
+		},
+		[20] = 
+		{
+		},
+	},
+	filename = "mash\\Mash-5.41 Diadem Artisanal FSH",
+	fish_3 = 
+	{
+		
+		{
+			HQamount = 0,
+			amount = 99999,
+			baits = "30281,",
+			collect = false,
+			cordial = true,
+			fish_loc = 10023,
+			fish_sk = "Default.lua",
+			fish_sknum = 2,
+			gearset = 0,
+			hold = false,
+			id = 32903,
+			job = 18,
+			move_inadvance = 0,
+			reserve = false,
+			skip = false,
+			skip_timer = false,
+			substr = "",
+			tasktype = "fish",
+			timerValue = 1143648,
+			tm_check_do = false,
+			v_lure = false,
+		},
+		
+		{
+			HQamount = 0,
+			amount = 99999,
+			baits = "30281,",
+			collect = false,
+			cordial = true,
+			fish_loc = 10023,
+			fish_sk = "Default.lua",
+			fish_sknum = 2,
+			gearset = 0,
+			hold = false,
+			id = 32899,
+			job = 18,
+			move_inadvance = 0,
+			reserve = false,
+			skip = false,
+			skip_timer = false,
+			substr = "",
+			tasktype = "fish",
+			timerValue = 927787,
+			tm_check_do = false,
+			v_lure = false,
+		},
+		
+		{
+			HQamount = 0,
+			amount = 99999,
+			baits = "30281,",
+			collect = false,
+			cordial = true,
+			fish_loc = 10023,
+			fish_sk = "Default.lua",
+			fish_sknum = 2,
+			gearset = 0,
+			hold = false,
+			id = 32895,
+			job = 18,
+			move_inadvance = 0,
+			reserve = false,
+			skip = false,
+			skip_timer = false,
+			substr = "",
+			tasktype = "fish",
+			timerValue = 985558,
+			tm_check_do = false,
+			v_lure = false,
+		},
+		
+		{
+			HQamount = 0,
+			amount = 99999,
+			baits = "30281,",
+			collect = false,
+			cordial = true,
+			fish_loc = 10023,
+			fish_sk = "Default.lua",
+			fish_sknum = 2,
+			gearset = 0,
+			hold = false,
+			id = 32894,
+			job = 18,
+			move_inadvance = 0,
+			reserve = false,
+			skip = false,
+			skip_timer = false,
+			substr = "",
+			tasktype = "fish",
+			timerValue = 1064763,
+			tm_check_do = false,
+			v_lure = false,
+		},
+	},
+	glist = 
+	{
+	},
+	orders = 
+	{
+	},
+	unspoil = 
+	{
+	},
+	unspoilTimeline = 
+	{
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		}, 
+		[0] = 
+		{
+		},
+	},
+}
+
+
+
+return tbl
