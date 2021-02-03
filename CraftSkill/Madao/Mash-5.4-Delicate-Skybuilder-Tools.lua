103327dc66bc90f1501203986975077e29b9c1c3
masshirodev <masshiro.iro.kk@gmail.com>
2021-02-03 06:02:59 -0400

    Adding delicate crafts skill profile

---
+++ CraftSkill/Madao/Mash-5.4-Delicate-Skybuilder-Tools.lua
@@ -0,0 +1,137 @@
+local tbl = 
+{
+	
+	{
+		control = 2700,
+		cp = 560,
+		craftmanship = 2810,
+		craftmanshipmax = 0,
+		id = -1,
+		level = 80,
+		name = "Stats Check",
+		name2 = "Cp:560 craft:2810 control: 2700 level:80",
+	},
+	
+	{
+		id = 100379,
+		name = "Muscle Memory",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 252,
+		name = "Inner Quiet",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 4574,
+		name = "Manipulation",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 19297,
+		name = "Veneration",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 4639,
+		name = "Waste Not II",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 100403,
+		name = "Groundwork",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 100403,
+		name = "Groundwork",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 100403,
+		name = "Groundwork",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 19004,
+		name = "Innovation",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 100299,
+		name = "Preparatory Touch",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 100299,
+		name = "Preparatory Touch",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 100299,
+		name = "Preparatory Touch",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 100299,
+		name = "Preparatory Touch",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 260,
+		name = "Great Strides",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 19004,
+		name = "Innovation",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 100339,
+		name = "Byregot's Blessing",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 100403,
+		name = "Groundwork",
+		singlecheck = true,
+		singleuse = true,
+	},
+}
+
+
+
+return tbl
