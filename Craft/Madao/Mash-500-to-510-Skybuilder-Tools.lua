c204fac78a32e7e65226f26fd612ef892813b759
masshirodev <masshiro.iro.kk@gmail.com>
2021-02-03 19:35:13 -0400

    DoH Skybuilder tools upgrade 510 added

---
+++ Craft/Madao/Mash-500-to-510-Skybuilder-Tools.lua
@@ -0,0 +1,771 @@
+local tbl = 
+{
+	backtoLocation = true,
+	btn = 
+	{
+		[32035] = 600,
+		[32036] = 300,
+		[32037] = 300,
+		[32038] = 300,
+		[32039] = 600,
+	},
+	buy = 
+	{
+	},
+	cant = 
+	{
+		[31758] = 60,
+		[31759] = 60,
+		[31760] = 55,
+		[31761] = 60,
+		[31762] = 60,
+		[31763] = 60,
+		[31764] = 60,
+		[31765] = 60,
+	},
+	craftLocHouseNum = 1,
+	craftLocType = "House",
+	ephs = 
+	{
+		[0] = 
+		{
+		},
+		[4] = 
+		{
+		},
+		[8] = 
+		{
+		},
+		[12] = 
+		{
+		},
+		[16] = 
+		{
+		},
+		[20] = 
+		{
+		},
+	},
+	filename = "mash\\Mash-500-to-510-Skybuilder-Tools",
+	fish_1 = 
+	{
+	},
+	fish_2 = 
+	{
+	},
+	fish_3 = 
+	{
+	},
+	foodId = 1030482,
+	foodName = "[HQ] Chili Crab",
+	glist = 
+	{
+		
+		{
+			HQamount = 0,
+			amount = 300,
+			collect = false,
+			cordial = true,
+			id = 32037,
+			isG2 = true,
+			job = 17,
+			mapid = 901,
+			p_detail = "Diadem",
+			point = 9999,
+			skillname = "Leveling.lua",
+			skip = false,
+			srange = 0,
+			started = 1612395079,
+			tasktype = 1,
+			timerValue = 1167755,
+			timeup = false,
+		},
+		
+		{
+			HQamount = 0,
+			amount = 300,
+			collect = false,
+			cordial = true,
+			id = 32036,
+			isG2 = true,
+			job = 17,
+			mapid = 901,
+			p_detail = "Diadem",
+			point = 9999,
+			skillname = "Leveling.lua",
+			skip = false,
+			srange = 0,
+			started = 1612395079,
+			tasktype = 1,
+			timerValue = 980550,
+			timeup = false,
+		},
+		
+		{
+			HQamount = 0,
+			amount = 600,
+			collect = false,
+			cordial = true,
+			id = 32035,
+			isG2 = true,
+			job = 17,
+			mapid = 901,
+			p_detail = "Diadem",
+			point = 9999,
+			skillname = "Leveling.lua",
+			skip = false,
+			srange = 0,
+			started = 1612395079,
+			tasktype = 1,
+			timerValue = 987802,
+			timeup = false,
+		},
+		
+		{
+			HQamount = 0,
+			amount = 600,
+			collect = false,
+			cordial = true,
+			id = 32043,
+			isG2 = true,
+			job = 16,
+			mapid = 901,
+			p_detail = "Diadem",
+			point = 9999,
+			skillname = "Leveling.lua",
+			skip = false,
+			srange = 0,
+			started = 1612395079,
+			tasktype = 1,
+			timerValue = 1179666,
+			timeup = false,
+		},
+		
+		{
+			HQamount = 0,
+			amount = 600,
+			collect = false,
+			cordial = true,
+			id = 32041,
+			isG2 = true,
+			job = 16,
+			mapid = 901,
+			p_detail = "Diadem",
+			point = 9999,
+			skillname = "Leveling.lua",
+			skip = false,
+			srange = 0,
+			started = 1612395079,
+			tasktype = 1,
+			timerValue = 1083377,
+			timeup = false,
+		},
+		
+		{
+			HQamount = 0,
+			amount = 600,
+			collect = false,
+			cordial = true,
+			id = 32040,
+			isG2 = true,
+			job = 16,
+			mapid = 901,
+			p_detail = "Diadem",
+			point = 9999,
+			skillname = "Leveling.lua",
+			skip = false,
+			srange = 0,
+			started = 1612395079,
+			tasktype = 1,
+			timerValue = 909091,
+			timeup = false,
+		},
+		
+		{
+			HQamount = 0,
+			amount = 300,
+			collect = false,
+			cordial = true,
+			id = 32038,
+			isG2 = true,
+			job = 17,
+			mapid = 901,
+			p_detail = "Diadem",
+			point = 9999,
+			skillname = "Leveling.lua",
+			skip = false,
+			srange = 0,
+			started = 1612395079,
+			tasktype = 1,
+			timerValue = 934653,
+			timeup = false,
+		},
+		
+		{
+			HQamount = 0,
+			amount = 600,
+			collect = false,
+			cordial = true,
+			id = 32042,
+			isG2 = true,
+			job = 16,
+			mapid = 901,
+			p_detail = "Diadem",
+			point = 9999,
+			skillname = "Leveling.lua",
+			skip = false,
+			srange = 0,
+			started = 1612395079,
+			tasktype = 1,
+			timerValue = 934836,
+			timeup = false,
+		},
+		
+		{
+			HQamount = 0,
+			amount = 600,
+			collect = false,
+			cordial = true,
+			id = 32039,
+			isG2 = true,
+			job = 17,
+			mapid = 901,
+			p_detail = "Diadem",
+			point = 9999,
+			skillname = "Leveling.lua",
+			skip = false,
+			srange = 0,
+			started = 1612395079,
+			tasktype = 1,
+			timerValue = 917651,
+			timeup = false,
+		},
+		
+		{
+			HQamount = 0,
+			amount = 300,
+			collect = false,
+			cordial = true,
+			id = 32044,
+			isG2 = true,
+			job = 16,
+			mapid = 901,
+			p_detail = "Diadem",
+			point = 9999,
+			skillname = "Leveling.lua",
+			skip = false,
+			srange = 0,
+			started = 1612395079,
+			tasktype = 1,
+			timerValue = 1131215,
+			timeup = false,
+		},
+	},
+	grind = 
+	{
+	},
+	listed = 
+	{
+		
+		{
+			amount = 60,
+			collect = false,
+			itemid = 31756,
+			job = 6,
+			name = "[80][ALC]Oddly Delicate Holy Water",
+			nq = false,
+			recipe = 34593,
+		},
+		
+		{
+			amount = 60,
+			collect = false,
+			itemid = 31757,
+			job = 7,
+			name = "[80][CUL]Oddly Delicate Shark Oil",
+			nq = false,
+			recipe = 34594,
+		},
+		
+		{
+			amount = 60,
+			collect = false,
+			itemid = 31755,
+			job = 5,
+			name = "[80][WVR]Oddly Delicate Rhea Cloth",
+			nq = false,
+			recipe = 34592,
+		},
+		
+		{
+			amount = 60,
+			collect = false,
+			itemid = 31754,
+			job = 4,
+			name = "[80][LTW]Oddly Delicate Gazelle Leather",
+			nq = false,
+			recipe = 34591,
+		},
+		
+		{
+			amount = 60,
+			collect = false,
+			itemid = 31753,
+			job = 3,
+			name = "[80][GSM]Oddly Delicate Celestine",
+			nq = false,
+			recipe = 34590,
+		},
+		
+		{
+			amount = 60,
+			collect = false,
+			itemid = 31752,
+			job = 2,
+			name = "[80][ARM]Oddly Delicate Wolfram Square",
+			nq = false,
+			recipe = 34589,
+		},
+		
+		{
+			amount = 60,
+			collect = false,
+			itemid = 31751,
+			job = 1,
+			name = "[80][BSM]Oddly Delicate Silver Gear",
+			nq = false,
+			recipe = 34588,
+		},
+		
+		{
+			amount = 60,
+			collect = false,
+			itemid = 31750,
+			job = 0,
+			name = "[80][CRP]Oddly Delicate Pine Lumber",
+			nq = false,
+			recipe = 34587,
+		},
+	},
+	miner = 
+	{
+		[32040] = 600,
+		[32041] = 600,
+		[32042] = 600,
+		[32043] = 600,
+		[32044] = 300,
+	},
+	orders = 
+	{
+		
+		{
+			HQingAuto = true,
+			HQingAutoVal = 25,
+			amount = 60,
+			amountInBag = 0,
+			amountInBagHQ = 0,
+			collect = false,
+			id = 34589,
+			ings = 
+			{
+				[31760] = 1,
+				[31996] = 5,
+				[32000] = 5,
+			},
+			ingsHQ = 
+			{
+				[31760] = 0,
+				[31996] = 0,
+				[32000] = 0,
+			},
+			item = 31752,
+			madaoPNum = 7,
+			madaoprofile = "mash\\Mash-5.4-Delicate-Skybuilder-Tools.lua",
+			name = "Oddly Delicate Wolfram Square",
+			noStop = true,
+			requiredcp = 0,
+			reserved = false,
+			shards = 
+			{
+				[15] = 3,
+				[17] = 3,
+			},
+			skillprofile = "None",
+			skilltype = 3,
+			skip = false,
+			usequick = false,
+		},
+		
+		{
+			HQingAuto = true,
+			HQingAutoVal = 25,
+			amount = 60,
+			amountInBag = 0,
+			amountInBagHQ = 0,
+			collect = false,
+			id = 34590,
+			ings = 
+			{
+				[31761] = 1,
+				[31995] = 5,
+				[31999] = 5,
+			},
+			ingsHQ = 
+			{
+				[31761] = 0,
+				[31995] = 0,
+				[31999] = 0,
+			},
+			item = 31753,
+			madaoPNum = 7,
+			madaoprofile = "mash\\Mash-5.4-Delicate-Skybuilder-Tools.lua",
+			name = "Oddly Delicate Celestine",
+			noStop = true,
+			requiredcp = 0,
+			reserved = false,
+			shards = 
+			{
+				[14] = 3,
+				[16] = 3,
+			},
+			skillprofile = "None",
+			skilltype = 3,
+			skip = false,
+			usequick = false,
+		},
+		
+		{
+			HQingAuto = true,
+			HQingAutoVal = 25,
+			amount = 60,
+			amountInBag = 0,
+			amountInBagHQ = 0,
+			collect = false,
+			id = 34591,
+			ings = 
+			{
+				[31762] = 1,
+				[31991] = 5,
+				[31993] = 5,
+			},
+			ingsHQ = 
+			{
+				[31762] = 0,
+				[31991] = 0,
+				[31993] = 0,
+			},
+			item = 31754,
+			madaoPNum = 7,
+			madaoprofile = "mash\\Mash-5.4-Delicate-Skybuilder-Tools.lua",
+			name = "Oddly Delicate Gazelle Leather",
+			noStop = true,
+			requiredcp = 0,
+			reserved = false,
+			shards = 
+			{
+				[16] = 3,
+				[17] = 3,
+			},
+			skillprofile = "None",
+			skilltype = 3,
+			skip = false,
+			usequick = false,
+		},
+		
+		{
+			HQingAuto = true,
+			HQingAutoVal = 25,
+			amount = 60,
+			amountInBag = 0,
+			amountInBagHQ = 0,
+			collect = false,
+			id = 34592,
+			ings = 
+			{
+				[31763] = 1,
+				[31994] = 5,
+				[31998] = 5,
+			},
+			ingsHQ = 
+			{
+				[31763] = 0,
+				[31994] = 0,
+				[31998] = 0,
+			},
+			item = 31755,
+			madaoPNum = 7,
+			madaoprofile = "mash\\Mash-5.4-Delicate-Skybuilder-Tools.lua",
+			name = "Oddly Delicate Rhea Cloth",
+			noStop = true,
+			requiredcp = 0,
+			reserved = false,
+			shards = 
+			{
+				[16] = 3,
+				[18] = 3,
+			},
+			skillprofile = "None",
+			skilltype = 3,
+			skip = false,
+			usequick = false,
+		},
+		
+		{
+			HQingAuto = true,
+			HQingAutoVal = 25,
+			amount = 60,
+			amountInBag = 0,
+			amountInBagHQ = 0,
+			collect = false,
+			id = 34593,
+			ings = 
+			{
+				[31764] = 1,
+				[31992] = 5,
+				[31997] = 5,
+			},
+			ingsHQ = 
+			{
+				[31764] = 0,
+				[31992] = 0,
+				[31997] = 0,
+			},
+			item = 31756,
+			madaoPNum = 7,
+			madaoprofile = "mash\\Mash-5.4-Delicate-Skybuilder-Tools.lua",
+			name = "Oddly Delicate Holy Water",
+			noStop = true,
+			requiredcp = 0,
+			reserved = false,
+			shards = 
+			{
+				[18] = 3,
+				[19] = 3,
+			},
+			skillprofile = "None",
+			skilltype = 3,
+			skip = false,
+			usequick = false,
+		},
+		
+		{
+			HQingAuto = true,
+			HQingAutoVal = 25,
+			amount = 60,
+			amountInBag = 0,
+			amountInBagHQ = 0,
+			collect = false,
+			id = 34594,
+			ings = 
+			{
+				[31765] = 1,
+				[31997] = 5,
+				[31998] = 5,
+			},
+			ingsHQ = 
+			{
+				[31765] = 0,
+				[31997] = 0,
+				[31998] = 0,
+			},
+			item = 31757,
+			madaoPNum = 7,
+			madaoprofile = "mash\\Mash-5.4-Delicate-Skybuilder-Tools.lua",
+			name = "Oddly Delicate Shark Oil",
+			noStop = true,
+			requiredcp = 0,
+			reserved = false,
+			shards = 
+			{
+				[14] = 3,
+				[19] = 3,
+			},
+			skillprofile = "None",
+			skilltype = 3,
+			skip = false,
+			usequick = false,
+		},
+		
+		{
+			HQingAuto = true,
+			HQingAutoVal = 25,
+			amount = 60,
+			amountInBag = 0,
+			amountInBagHQ = 0,
+			collect = false,
+			id = 34588,
+			ings = 
+			{
+				[31759] = 1,
+				[31996] = 5,
+				[31999] = 5,
+			},
+			ingsHQ = 
+			{
+				[31759] = 0,
+				[31996] = 0,
+				[31999] = 0,
+			},
+			item = 31751,
+			madaoPNum = 7,
+			madaoprofile = "mash\\Mash-5.4-Delicate-Skybuilder-Tools.lua",
+			name = "Oddly Delicate Silver Gear",
+			noStop = true,
+			requiredcp = 0,
+			reserved = false,
+			shards = 
+			{
+				[14] = 3,
+				[17] = 3,
+			},
+			skillprofile = "None",
+			skilltype = 3,
+			skip = false,
+			usequick = false,
+		},
+		
+		{
+			HQingAuto = true,
+			HQingAutoVal = 25,
+			amount = 60,
+			amountInBag = 0,
+			amountInBagHQ = 0,
+			collect = false,
+			id = 34587,
+			ings = 
+			{
+				[31758] = 1,
+				[31991] = 5,
+				[31995] = 5,
+			},
+			ingsHQ = 
+			{
+				[31758] = 0,
+				[31991] = 0,
+				[31995] = 0,
+			},
+			item = 31750,
+			madaoPNum = 7,
+			madaoprofile = "mash\\Mash-5.4-Delicate-Skybuilder-Tools.lua",
+			name = "Oddly Delicate Pine Lumber",
+			noStop = true,
+			requiredcp = 0,
+			reserved = false,
+			shards = 
+			{
+				[15] = 3,
+				[16] = 3,
+			},
+			skillprofile = "None",
+			skilltype = 3,
+			skip = false,
+			usequick = false,
+		},
+	},
+	potId = "1027958",
+	potName = "[HQ] Commanding Craftsman's Syrup",
+	profileRepeat = false,
+	profileType = "OneTime",
+	profileType_num = 1,
+	reserve = 
+	{
+		[31758] = 1,
+		[31759] = 1,
+		[31760] = 1,
+		[31761] = 1,
+		[31762] = 1,
+		[31763] = 1,
+		[31764] = 1,
+		[31765] = 1,
+		[31991] = 1,
+		[31992] = 1,
+		[31993] = 1,
+		[31994] = 1,
+		[31995] = 1,
+		[31996] = 1,
+		[31997] = 1,
+		[31998] = 1,
+		[31999] = 1,
+		[32000] = 1,
+		[34587] = 1,
+		[34588] = 1,
+		[34589] = 1,
+		[34590] = 1,
+		[34591] = 1,
+		[34592] = 1,
+		[34593] = 1,
+		[34594] = 1,
+	},
+	reserve2 = 
+	{
+		[31758] = 1,
+		[31759] = 1,
+		[31760] = 1,
+		[31761] = 1,
+		[31762] = 1,
+		[31763] = 1,
+		[31764] = 1,
+		[31765] = 1,
+		[31991] = 1,
+		[31992] = 1,
+		[31993] = 1,
+		[31994] = 1,
+		[31995] = 1,
+		[31996] = 1,
+		[31997] = 1,
+		[31998] = 1,
+		[31999] = 1,
+		[32000] = 1,
+		[34587] = 1,
+		[34588] = 1,
+		[34589] = 1,
+		[34590] = 1,
+		[34591] = 1,
+		[34592] = 1,
+		[34593] = 1,
+		[34594] = 1,
+	},
+	s_c = 
+	{
+	},
+	s_g = 
+	{
+	},
+	scripitemdemands = 
+	{
+	},
+	shards = 
+	{
+		[14] = 540,
+		[15] = 360,
+		[16] = 720,
+		[17] = 540,
+		[18] = 360,
+		[19] = 360,
+	},
+	skill = 
+	{
+		dskName = "None",
+		skeName = "None",
+		skillType = "Auto",
+	},
+	tmlist = 
+	{
+	},
+	unspoil = 
+	{
+	},
+	unspoilTimeline = 
+	{
+	},
+	useFood = true,
+	usePot = true,
+	withdraw = 
+	{
+	},
+	withpdata = true,
+}
+
+
+
+return tbl
