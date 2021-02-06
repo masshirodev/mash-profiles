28132af73be5c6f8a1542eb515912a055804c630
masshirodev <masshiro.iro.kk@gmail.com>
2021-02-06 02:36:02 -0400

    5.45 Skybuilder dol upgrade mats

---
+++ Gather/Madao/Mash-5.45 Skybuilders DoL Tools 500-510.lua
@@ -0,0 +1,249 @@
+local tbl = 
+{
+	eph = 
+	{
+	},
+	ephs = 
+	{
+		[0] = 
+		{
+		},
+		[4] = 
+		{
+		},
+		[8] = 
+		{
+		},
+		[12] = 
+		{
+		},
+		[16] = 
+		{
+		},
+		[20] = 
+		{
+		},
+	},
+	filename = "Mash-5.45-Skybuilders-Tools-500-510",
+	fish_1 = 
+	{
+	},
+	fish_3 = 
+	{
+		
+		{
+			HQamount = 0,
+			amount = 200,
+			baits = "30136,",
+			collect = true,
+			cordial = true,
+			fish_loc = 143,
+			fish_sk = "Default.lua",
+			fish_sknum = 2,
+			hold = false,
+			id = 31771,
+			job = 18,
+			reserve = false,
+			skip = false,
+			substr = "",
+			tasktype = "fish",
+			timerValue = 1063143,
+			tm_check_do = false,
+			v_lure = false,
+		},
+		
+		{
+			HQamount = 0,
+			amount = 200,
+			baits = "30136,",
+			collect = true,
+			cordial = true,
+			fish_loc = 144,
+			fish_sk = "Default.lua",
+			fish_sknum = 2,
+			hold = false,
+			id = 31770,
+			job = 18,
+			reserve = false,
+			skip = false,
+			substr = "",
+			tasktype = "fish",
+			timerValue = 1051515,
+			tm_check_do = false,
+			v_lure = false,
+		},
+	},
+	glist = 
+	{
+		
+		{
+			HQamount = 60,
+			amount = 60,
+			collect = false,
+			cordial = true,
+			hold = false,
+			id = 31768,
+			job = 16,
+			mapid = 402,
+			name = "Oddly Delicate Adamantite Ore",
+			okgp = 0,
+			p_detail = "[MIN] Azys Lla / Gamma Quadrant / X:35 Y:29",
+			point = 764,
+			reserve = false,
+			skillname = "Collective_Default.lua",
+			skillnum = 3,
+			skip = false,
+			tasktype = "gather",
+			timerValue = 1033708,
+		},
+		
+		{
+			HQamount = 60,
+			amount = 0,
+			collect = false,
+			cordial = true,
+			hold = false,
+			id = 31766,
+			job = 17,
+			mapid = 402,
+			name = "Oddly Delicate Feather",
+			okgp = 0,
+			p_detail = "[BTN] Azys Lla / Antithesis / X: 10 Y:26",
+			point = 765,
+			reserve = false,
+			skillname = "Collective_Default.lua",
+			skillnum = 3,
+			skip = false,
+			tasktype = "gather",
+			timerValue = 935725,
+		},
+		
+		{
+			HQamount = 750,
+			amount = 0,
+			collect = false,
+			cordial = true,
+			gearset = 0,
+			hidden = false,
+			hold = false,
+			id = 31767,
+			job = 17,
+			name = "Oddly Delicate Birch Log",
+			notinlistcaseid = 0,
+			okgp = 0,
+			p_detail = "[BTN] The Diadem / The Diadem / X:27 Y:16",
+			point = 806,
+			reserve = false,
+			skillname = "mash\\Mash-HQ Priority.lua",
+			skillnum = 14,
+			skip = false,
+			tasktype = "gather",
+			timerValue = 901400,
+		},
+		
+		{
+			HQamount = 750,
+			amount = 0,
+			collect = false,
+			cordial = true,
+			hold = false,
+			id = 31769,
+			job = 16,
+			name = "Oddly Delicate Raw Jade",
+			okgp = 0,
+			p_detail = "[MIN] The Diadem / The Diadem / X:27 Y:17",
+			point = 803,
+			reserve = false,
+			skillname = "mash\\Mash-HQ Priority.lua",
+			skillnum = 14,
+			skip = false,
+			tasktype = "gather",
+			timerValue = 902673,
+		},
+	},
+	orders = 
+	{
+	},
+	unspoil = 
+	{
+	},
+	unspoilTimeline = 
+	{
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		}, 
+		[0] = 
+		{
+		},
+	},
+}
+
+
+
+return tbl
