1809e8ce2a7c7cd1e11cb1bda5e0db58849e9815
masshirodev <masshiro.iro.kk@gmail.com>
2021-12-25 10:33:13 -0300

    adding blerkel's profiles

---
+++ CraftSkill/Madao/6.0 blerkel Lvl 90 Purple Scrip.lua
@@ -0,0 +1,105 @@
+local tbl = 
+{
+	
+	{
+		id = 100379,
+		name = "Muscle Memory",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 4574,
+		name = "Manipulation",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 4639,
+		name = "Waste Not II",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 19297,
+		name = "Veneration",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 100403,
+		name = "Groundwork",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 19004,
+		name = "Innovation",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 100299,
+		name = "Preparatory Touch",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 100299,
+		name = "Preparatory Touch",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 100299,
+		name = "Preparatory Touch",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 100299,
+		name = "Preparatory Touch",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 19004,
+		name = "Innovation",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 260,
+		name = "Great Strides",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 100339,
+		name = "Byregot's Blessing",
+		singlecheck = true,
+		singleuse = true,
+	},
+	
+	{
+		id = 100403,
+		name = "Groundwork",
+		singlecheck = true,
+		singleuse = true,
+	},
+}
+
+
+
+return tbl
